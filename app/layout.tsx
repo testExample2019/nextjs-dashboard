30d3803c625424184bd930985bbe10a826b288fa
eugene.martynenko <eugene.martynenko@dataart.com>
2025-02-04 16:34:57 +0200

    meta updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ToastProvider } from "@/app/lib/contexts/toast-context";
 import { StepsProvider } from "@/app/lib/contexts/steps-context";
 import StepCustomCard from "@/app/ui/components/step-custom-card";
 import "@/app/ui/global.css";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: {
@@ -13,7 +14,7 @@ export const metadata: Metadata = {
     default: "DirectLend Platform",
   },
   description:
-    "We’re excited to have you on board. Our platform is designed to cater to both Borrowers and Lenders, ensuring a seamless experience for all users",
+    "We’re excited to have you on board. Our platform is designed to cater to both borrowers and lenders, ensuring a seamless experience for all users",
 };
 
 export default function RootLayout({
@@ -23,6 +24,17 @@ export default function RootLayout({
 }) {
   return (
     <NextStepProvider>
+      <Head>
+        <meta property="og:title" content="DirectLend Platform" />
+        <meta
+          property="og:description"
+          content="We’re excited to have you on board. Our platform is designed to cater to both borrowers and lenders, ensuring a seamless experience for all users"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://yourdomain.com" />
+        <meta property="og:image" content="/opengraph-image.png" />
+        <meta property="og:site_name" content="DirectLend Platform" />
+      </Head>
       <html lang="en">
         <body
           id={"app-body"}
